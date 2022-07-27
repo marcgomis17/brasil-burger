d76f2a49af3d89dcc3bf9c7a350d2beb7f81b26a
Marc Gomis <70306058+marcgomis17@users.noreply.github.com>
2022-07-27 21:16:11 +0000

    Client module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'client', pathMatch: 'full' },
+  { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) },
+  { path: 'auth', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/client/client.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientRoutingModule } from './client-routing.module';
+import { ClientSharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    ClientRoutingModule,
+    ClientSharedModule
+  ]
+})
+export class ClientModule { }
